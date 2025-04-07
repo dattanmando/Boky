790e11168eba24a607d4046d9d71d415e8adf0ce
Tanner Chamberlain <tannerchamberlain8@gmail.com>
2025-04-07 06:40:10 -0600

    connect mongod

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
+dotenv.config();
+console.log("Connecting to MongoDB...", process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 const db = mongoose.connection;
 
